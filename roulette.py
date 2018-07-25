758e87bb0cf1170fe6f3ea03c4d53d4120041b3e
Benjamin Diaz <benjadf@outlook.com>
2018-07-25 12:02:20 -0400

    You can now bet to a color

--- roulette.py
+++ roulette.py
@@ -50,12 +50,38 @@ def spinOnce():
     x=spin()
     print("NUMBER:  "+roul.array[x].n, "    COLOR:  "+roul.array[x].c)
 
+def colorBet(amount):
+    print("Press B for Black or R for Red")
+    color = input()
+    x=spin()
+    roul=roulette()
+    print("NUMBER:  "+roul.array[x].n, "    COLOR:  "+roul.array[x].c)
+    if color == "B" or color == "b":
+        if roul.array[x].c=="black":
+            return int(amount)*2
+        else:
+            return 0
+    elif color == "r" or color == "R":
+        if roul.array[x].c == "red":
+            return int(amount)*2
+        else:
+            return 0
+
+
 while(1):
-    print("Please input the number of roulette spins for which you want to check maximum color repetitions. Press 0 to print roulette. Press S to spin once.")
+    print("Please input the number of roulette spins for which you want to check maximum color repetitions. Press B to bet. Press 0 to print roulette. Press S to spin once.")
     number=input()        
     if str(number) == "P" or str(number)  == "p":
         printRoulette()
     elif str(number) == "s" or str(number)  == "S":
         spinOnce()
+    elif str(number) == "b" or str(number)  == "B":
+        print("How much do you want to bet?")
+        amount=input()
+        bet=colorBet(amount)
+        if bet == 0:
+            print("You lost")
+        else:
+            print("You have won and have now: "+str(bet))
     else:
         print("The maximum number of repetitions is: "+str(getRepetitions(number)))
